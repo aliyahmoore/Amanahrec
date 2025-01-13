2d6eef0756f287df0e8390721de2dd177f87ef77
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-13 14:07:33 -0600

    ran bundle install and migration

--- db/schema.rb
+++ db/schema.rb
@@ -29,5 +29,4 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_13_002925) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
->>>>>>> origin/main
 end
