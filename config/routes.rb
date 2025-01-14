52c98c2f891ef8cd7c015798c8964bc2f89f3b5a
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-14 11:12:03 -0600

    Save work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   get "/about", to: "pages#about"
+  resources :media, only: [ :index, :show ]
+
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
