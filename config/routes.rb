f9a709b90132ce5f364fcf75bbb0495d50787b46
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-14 14:42:10 -0600

    added about views with minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "/about", to: "pages#about"
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
