1b01b0c2ae4fd097e2229e3471d79653bacda881
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-14 13:07:31 -0600

    Post migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_14_184049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", default: "", null: false
+    t.string "phone_number"
+    t.boolean "member", default: false, null: false
+    t.string "gender"
+    t.string "age_range"
+    t.string "ethnicity"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 end
