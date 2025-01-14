5f17d33fc44a44b941a695d1f602533f9d3f59b4
Saba Negeye <saban@Sabas-Air.hsd1.mn.comcast.net>
2025-01-14 11:46:29 -0600

    added schema.rb to make CI test pass

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,3 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_16_193437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-ActiveRecord::Schema[7.2].define(version: 0) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-end
