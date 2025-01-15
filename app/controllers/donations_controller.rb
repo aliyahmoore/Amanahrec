734bca97b9ae5f7585d14544c817d43abdc3a291
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-14 23:38:50 -0600

    Rubocop

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -4,10 +4,10 @@ class DonationsController < ApplicationController
   def create
     Stripe::PaymentIntent.create({
       amount: (params[:amount].to_i * 100), # Amount in cents
-      currency: 'usd',
+      currency: "usd",
       payment_method: params[:payment_method_id],
       confirm: true,
-      automatic_payment_methods: { enabled: true, allow_redirects: 'never' }
+      automatic_payment_methods: { enabled: true, allow_redirects: "never" }
     })
     redirect_to root_path, notice: "Thank you for your donation!"
   rescue Stripe::CardError => e
