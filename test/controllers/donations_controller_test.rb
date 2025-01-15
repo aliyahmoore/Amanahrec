d259e8f677b7a3633be2e5aaf84a076f0ddc9ed0
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-15 00:09:39 -0600

    Rubocop again

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -1,13 +1,13 @@
-require 'test_helper'
+require "test_helper"
 
 class DonationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get new_donation_url  
+    get new_donation_url
     assert_response :success
   end
 
   test "should create donation" do
-    post donations_url, params: { donation: { amount: 1000 } } 
+    post donations_url, params: { donation: { amount: 1000 } }
     assert_redirected_to root_path
   end
 end
