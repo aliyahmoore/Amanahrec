6b223dad74e117906dcb476e0fea23d90c0a726a
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-14 23:52:35 -0600

    Update with correct routes and donation amount

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class DonationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get donations_new_url
+    get new_donation_url  
     assert_response :success
   end
 
-  test "should get create" do
-    get donations_create_url
-    assert_response :success
+  test "should create donation" do
+    post donations_url, params: { donation: { amount: 1000 } } 
+    assert_redirected_to root_path
   end
 end
