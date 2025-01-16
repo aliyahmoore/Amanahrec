9cc94069c51a0c725afb60b4d0f6ee237960a28a
peterlee597 <peterlee597@gmail.com>
2025-01-15 21:33:05 -0600

    created events model with appropriate fields

---
+++ db/migrate/20250116033146_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.time :time
+      t.date :date
+      t.string :location
+      t.date :rsvp_deadline
+      t.boolean :childcare
+      t.string :sponsors
+      t.decimal :cost, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
