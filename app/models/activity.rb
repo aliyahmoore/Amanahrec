dc1f69586236f7d9522ffa05011d23dea8d0b9b3
peterlee597 <peterlee597@gmail.com>
2025-01-16 17:27:04 -0600

    Created Activities Model and Added Validations

---
+++ app/models/activity.rb
@@ -0,0 +1,10 @@
+class Activity < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :time, presence: true
+    validates :date, presence: true
+    validates :location, presence: true
+    validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :cost, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+    validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+end
