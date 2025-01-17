00e1b736b5752a4ff68a66f373f696963746d9e3
peterlee597 <peterlee597@gmail.com>
2025-01-16 18:44:30 -0600

    Created events model along with validations

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ApplicationRecord
+    # Validations
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :time, presence: true
+    validates :date, presence: true
+    validates :end_date, presence: true
+    validates :location, presence: true
+    validates :rsvp_deadline, presence: true
+    validates :sponsors, presence: true
+    validates :cost, numericality: { greater_than_or_equal_to: 0 }
+
+  # Additional validations or associations can be added as needed
+end
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,37 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_21_200642) do
     t.index ["user_id"], name: "index_testimonials_on_user_id"
   end
 
+  create_table "activities", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.time "time"
+    t.date "date"
+    t.string "location"
+    t.integer "capacity"
+    t.text "what_to_bring"
+    t.text "rules"
+    t.text "notes"
+    t.decimal "cost", precision: 8, scale: 2
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.time "time"
+    t.date "date"
+    t.date "end_date"
+    t.string "location"
+    t.date "rsvp_deadline"
+    t.boolean "childcare"
+    t.string "sponsors"
+    t.decimal "cost", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 255, null: false
     t.string "last_name", limit: 255, null: false
