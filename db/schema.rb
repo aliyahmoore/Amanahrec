fa27ef7a1570a7ff0372971b74ffc4a6d00f3ffc
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-16 20:42:57 -0600

    Ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_14_184049) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_16_193437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "media", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 255, null: false
