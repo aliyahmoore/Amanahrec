4dd33f5ee98cc54574f9a565ab55fb6dec9da7b1
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-19 13:46:14 -0600

    rebase

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :media, only: [ :index, :show ]
+  resources :media, only: [ :index ]
   # Defines the root path route ("/")
   # root "posts#index"
   root "home#index"
