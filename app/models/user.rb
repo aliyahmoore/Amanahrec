cd5b4b27873cd15145e454344ebaed0a906cdaf9
Saba Negeye <saban@Sabas-MacBook-Air.local>
2025-01-20 16:14:56 -0600

    added role model and users foregin key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :testimonials, dependent: :destroy
+  belongs_to :role
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +11,12 @@ class User < ApplicationRecord
 
 
   validates :first_name, :last_name, :email, :phone_number, :gender, :ethnicity, presence: true
+
+  def admin?
+    role.name == 'admin'
+  end
+
+  def user?
+    role.name == 'user'
+  end
 end
