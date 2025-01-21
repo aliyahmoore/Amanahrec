35553a76431ce03967b9365f62a8b367fab52b30
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-21 13:32:56 -0600

    Updated mvc for description

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,5 @@
 class Medium < ApplicationRecord
   validates :name, presence: true
   validates :link, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, allow_nil: true, allow_blank: true
 end
