fe5ac67e170df2f5d62d3bdc26bc58eac3293f6c
peterlee597 <peterlee597@gmail.com>
2025-01-21 10:14:44 -0600

    Created Joint table and Added Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_18_200339) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_21_161050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_18_200339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "event_datetime", precision: nil
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
+    t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
   create_table "users", force: :cascade do |t|
