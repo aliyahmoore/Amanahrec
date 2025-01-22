68266f908f2a7cb22a3dc8ba4fa0ca05a4ca1bb6
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-21 22:25:17 -0600

    Created partial form for edit and new media posts for better organization; embedded iframe for media post link and removed description.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,3 @@
 class Medium < ApplicationRecord
   validates :name, :link, :published_date, :organization_name, presence: true
-  validates :description, presence: true, allow_nil: true, allow_blank: true
 end
