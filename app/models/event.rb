170d5693e444256dd14fc1cd9304f7ec055513da
peterlee597 <peterlee597@gmail.com>
2025-01-22 13:21:06 -0600

    debugging and fixed inconsistencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
     validates :title, presence: true
     validates :description, presence: true
-    validates :datetime, presence: true
-    validates :end_datetime, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
     validates :location, presence: true
     validates :rsvp_deadline, presence: true
     validates :cost, numericality: { greater_than_or_equal_to: 0 }
