ac714194086c5dbc32b434c098d474035c05bad6
Saba Negeye <saban@Sabas-Air.hsd1.mn.comcast.net>
2025-01-23 13:24:50 -0600

    moved JS into its own file and linked it to fix reload error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @testimonials = Testimonial.where(approved: true).order(created_at: :desc)
-    render 'pages/home'
+    render "pages/home"
   end
 end
