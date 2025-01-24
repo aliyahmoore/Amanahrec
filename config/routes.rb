2b416efbc5d560c32354054ff0e47f10f20fb77b
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-24 00:29:21 -0600

    Moved Our Partners to a separate page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get "pages/home"
+  get "pages/partners"
   get "pages/about"
   get "pages/calendar"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
