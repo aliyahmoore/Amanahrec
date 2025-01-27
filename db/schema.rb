dfeaa37a5063cfd7b430391f9dc05adf05db3d9c
Saba Negeye <saban@Sabas-MacBook-Air.local>
2025-01-27 13:32:54 -0600

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,67 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_215558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "location"
-    t.datetime "rsvp_deadline"
-    t.boolean "childcare"
-    t.string "sponsors"
-    t.decimal "cost", precision: 8, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "testimonials", force: :cascade do |t|
-    t.text "text"
-    t.boolean "approved"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_testimonials_on_user_id"
-  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -103,8 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
   create_table "activities", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.time "time"
-    t.date "date"
+    t.datetime "date"
     t.string "location"
     t.integer "capacity"
     t.text "what_to_bring"
@@ -114,6 +55,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "activities_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.index ["activity_id", "user_id"], name: "index_activities_users_on_activity_id_and_user_id"
+    t.index ["user_id", "activity_id"], name: "index_activities_users_on_user_id_and_activity_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -126,13 +74,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
     t.boolean "childcare"
     t.string "sponsors"
     t.decimal "cost", precision: 8, scale: 2
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "location"
-    t.datetime "rsvp_deadline"
-    t.boolean "childcare"
-    t.string "sponsors"
-    t.decimal "cost", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -144,11 +85,19 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
     t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
-  create_table "events_users", id: false, force: :cascade do |t|
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.text "text"
+    t.boolean "approved"
     t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
-    t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_testimonials_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -176,8 +125,4 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_004342) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "testimonials", "users"
   add_foreign_key "users", "roles"
-
-  add_foreign_key "testimonials", "users"
-
-  add_foreign_key "payments", "users"
 end
