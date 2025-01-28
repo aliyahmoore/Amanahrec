3552e777aa518a73d35a279be0ff3ae0c93af7ca
Saba Negeye <saban@Sabas-Air.hsd1.mn.comcast.net>
2025-01-28 14:00:02 -0600

    event validations and controller logic to take general_registration_start into account

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,9 @@ class EventsController < ApplicationController
         # Members can see all events
         @events = Event.all
       else
-        # Non-members can only see events without early access or events within the early access window
-        early_access_days = Event.minimum(:early_access_days) || 0 # Default to 0 if nil
-      @events = Event.where(early_access_for_members: false)
-                     .or(Event.where('start_date <= ? AND early_access_for_members = ?', Time.current + early_access_days.days, true))
+        # Non-members can only see events available to the general public
+        @events = Event.where(early_access_for_members: false)
+                       .or(Event.where('general_registration_start <= ?', Time.current))
       end
     end
 
@@ -72,7 +71,7 @@ class EventsController < ApplicationController
     def event_params
       params.require(:event).permit(
         :title, :description, :start_date, :end_date, :location,
-        :rsvp_deadline, :childcare,:early_access_for_members,:early_access_days,:sponsors, :cost, images: []
+        :rsvp_deadline, :childcare,:early_access_for_members,:early_access_days,:general_registration_start,:sponsors, :cost, images: []
       )
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -10,5 +10,15 @@ class Event < ApplicationRecord
     validates :rsvp_deadline, presence: true
     validates :cost, numericality: { greater_than_or_equal_to: 0 }
     validates :early_access_days, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+    validates :general_registration_start, presence: true, if: -> { early_access_for_members? }
+    
+    
+    
+    
+    # Method to calculate the early access start date
+  def early_access_start_date
+    return nil unless general_registration_start && early_access_days
 
+    general_registration_start - early_access_days.days
+  end
 end
