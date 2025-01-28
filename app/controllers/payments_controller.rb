ecd2e87a00c8a8d2d1e919df409f3daa16116ee5
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2025-01-27 19:57:28 -0600

    Rubocop again

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,5 @@
 class PaymentsController < ApplicationController
-  before_action :set_event, only: [ :create, :success]
+  before_action :set_event, only: [ :create, :success ]
 
   def create
   # Ensure the user is logged in before registering
