151a7f7e206236a037234eb8f3381eb6eebdf891
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-28 12:25:39 -0600

    Updated cards to feature side by side

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 # config/importmap.rb
-pin "media_toggle", to: "media_toggle.js"
 pin "testimonial_slider", to: "testimonial_slider.js"
 pin "testimonial_slider", to: "testimonial_slider.js"
