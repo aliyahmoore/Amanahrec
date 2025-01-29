82bf0f4be947c21cc4a46116d28ff8153db30b1f
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-28 23:02:29 -0600

    schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_28_192605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "published_date"
+    t.string "organization_name"
+  end
+
+  create_table "activities_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.index ["activity_id", "user_id"], name: "index_activities_users_on_activity_id_and_user_id"
+    t.index ["user_id", "activity_id"], name: "index_activities_users_on_user_id_and_activity_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -69,6 +78,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
     t.decimal "cost", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "early_access_for_members"
+    t.integer "early_access_days"
+    t.datetime "general_registration_start"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
@@ -78,37 +90,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
     t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
-  create_table "media", force: :cascade do |t|
-    t.string "name"
-    t.string "link"
-    t.text "description"
+  create_table "media_mentions", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "link", null: false
+    t.date "published_date", null: false
+    t.string "organization_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "published_date"
-    t.string "organization_name"
-  end
-
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.string "stripe_payment_id"
-    t.decimal "amount"
-    t.string "status"
-    t.bigint "event_id", null: false
-    t.bigint "user_id", null: false
-    t.boolean "is_recurring"
-    t.string "recurring_type"
-    t.datetime "payment_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_payments_on_event_id"
-    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -149,8 +137,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "payments", "events"
-  add_foreign_key "payments", "users"
   add_foreign_key "testimonials", "users"
   add_foreign_key "users", "roles"
 end
