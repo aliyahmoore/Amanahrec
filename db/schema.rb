c7296991af053041a50e80c95a3c815d5ec4a45c
Nadia Kane <nadia.kane26@gmail.com>
2025-01-29 14:42:47 -0600

    Get ready for demo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "early_access_for_members"
+    t.integer "early_access_days"
+    t.datetime "general_registration_start"
   end
 
   create_table "events", force: :cascade do |t|
@@ -91,12 +94,31 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
     t.string "organization_name"
   end
 
+  create_table "media_mentions", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "link", null: false
+    t.date "published_date", null: false
+    t.string "organization_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "meetings", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -152,7 +174,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_23_183026) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "payments", "events"
+  add_foreign_key "memberships", "users"
   add_foreign_key "payments", "users"
   add_foreign_key "testimonials", "users"
   add_foreign_key "users", "roles"
