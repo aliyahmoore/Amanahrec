72187cdf663d2ea2254e64e34e62a08cd8b38c78
Nadia Kane <nadia.kane26@gmail.com>
2025-01-30 10:06:32 -0600

    Register event button capitalize

--- db/migrate/20250130013724_add_early_access_to_activities.rb
+++
@@ -1,7 +0,0 @@
-class AddEarlyAccessToActivities < ActiveRecord::Migration[7.2]
-  def change
-    add_column :activities, :early_access_for_members, :boolean
-    add_column :activities, :early_access_days, :integer
-    add_column :activities, :general_registration_start, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_30_143233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,9 +78,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
     t.boolean "early_access_for_members", default: false, null: false
     t.integer "early_access_days"
     t.datetime "general_registration_start"
-    t.boolean "early_access_for_members", default: false, null: false
-    t.integer "early_access_days"
-    t.datetime "general_registration_start"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
@@ -122,16 +119,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
-  create_table "memberships", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "status"
+    t.string "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_memberships_on_user_id"
@@ -146,13 +134,21 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
     t.string "recurring_type"
     t.string "paymentable_type", null: false
     t.bigint "paymentable_id", null: false
-    t.string "paymentable_type", null: false
-    t.bigint "paymentable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["paymentable_type", "paymentable_id"], name: "index_payments_on_paymentable"
-    t.index ["paymentable_type", "paymentable_id"], name: "index_payments_on_paymentable"
     t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "registrable_type", null: false
+    t.bigint "registrable_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["registrable_type", "registrable_id"], name: "index_registrations_on_registrable"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -194,8 +190,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_29_182311) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "memberships", "users"
-  add_foreign_key "memberships", "users"
   add_foreign_key "payments", "users"
+  add_foreign_key "registrations", "users"
   add_foreign_key "testimonials", "users"
   add_foreign_key "users", "roles"
 end
