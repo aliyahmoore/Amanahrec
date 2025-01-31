5a1a901fd68cef28672284a823784ab2f12c1e09
Nadia Kane <nadia.kane26@gmail.com>
2025-01-31 12:50:53 -0600

    Clean up again

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -65,6 +65,6 @@ class BoardsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def board_params
-      params.require(:board).permit(:name, :position, :description, :image )
+      params.require(:board).permit(:name, :position, :description, :image)
     end
 end
