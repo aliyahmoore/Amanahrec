9793f2a3ddfcb62b1ffe6bb688136a1992807bf7
Aliyah Moore <aliyahm.moore@yahoo.com>
2025-01-31 10:28:30 -0600

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :media_mentions
+  resources :media_mentions, only: [ :index, :new, :create, :edit, :update, :destroy ]
   get "/about", to: "pages#about"
   # Defines the root path route ("/")
   # root "posts#index"
