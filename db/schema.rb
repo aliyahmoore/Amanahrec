74649f89bbe0edcd4397e6b022c5390f069c472d
peterlee597 <peterlee597@gmail.com>
2025-01-30 23:02:04 -0600

    redoing this branch

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_31_180455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "registration_status", ["pending", "successful", "failed"]
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -94,44 +98,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_31_180455) do
     t.string "organization_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "memberships", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "status"
-    t.string "stripe_customer_id"
-    t.string "stripe_subscription_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.string "stripe_payment_id"
-    t.decimal "amount"
-    t.string "status"
-    t.bigint "user_id", null: false
-    t.boolean "is_recurring"
-    t.string "recurring_type"
-    t.string "paymentable_type", null: false
-    t.bigint "paymentable_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["paymentable_type", "paymentable_id"], name: "index_payments_on_paymentable"
-    t.index ["user_id"], name: "index_payments_on_user_id"
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "registrable_type", null: false
-    t.bigint "registrable_id", null: false
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["registrable_type", "registrable_id"], name: "index_registrations_on_registrable"
-    t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
