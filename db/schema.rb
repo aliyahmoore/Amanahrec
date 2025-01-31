375e84435ddac03125c1b1b83f977fa74a37083b
Nadia Kane <nadia.kane26@gmail.com>
2025-01-30 18:18:37 -0600

    Clean up schema

--- db/schema.rb
+++ db/schema.rb
@@ -80,36 +80,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_143233) do
     t.datetime "general_registration_start"
   end
 
-  create_table "events_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
-    t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
-  end
-
-  create_table "media", force: :cascade do |t|
-    t.string "name"
-    t.string "link"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.date "published_date"
-    t.string "organization_name"
-  end
-
   create_table "media_mentions", force: :cascade do |t|
     t.string "name", null: false
     t.string "link", null: false
     t.date "published_date", null: false
     t.string "organization_name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
