f86356f42ffbb01e1f117a4683f35433fe521ae1
Nadia Kane <nadia.kane26@gmail.com>
2025-01-31 10:13:06 -0600

    Handling multiple payments status, more error handling, Reduced to smaller methods

--- app/services/subscription_cancellation_service.rb
+++ app/services/subscription_cancellation_service.rb
@@ -3,12 +3,16 @@ class SubscriptionCancellationService
     @user = user
   end
 
+  # Cancel subscription
   def cancel
+    # Retrieve Stripe subscription 
     stripe_subscription = Stripe::Subscription.retrieve(@user.membership.stripe_subscription_id)
     stripe_subscription.cancel
 
+    # Update membership status
     @user.membership.update(status: 'canceled')
 
+    # Update payment statusq
     payment = @user.payments.find_by(is_recurring: true, status: 'succeeded')
     payment.update!(status: 'canceled') if payment
 
