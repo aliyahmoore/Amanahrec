c8675fa21689092656bf5f25710d67e7c31c8815
Nadia Kane <nadia.kane26@gmail.com>
2025-01-30 23:53:59 -0600

    Refractor payments controller with services

---
+++ app/services/subscription_cancellation_service.rb
@@ -0,0 +1,20 @@
+class SubscriptionCancellationService
+  def initialize(user)
+    @user = user
+  end
+
+  def cancel
+    stripe_subscription = Stripe::Subscription.retrieve(@user.membership.stripe_subscription_id)
+    stripe_subscription.cancel
+
+    @user.membership.update(status: 'canceled')
+
+    payment = @user.payments.find_by(is_recurring: true, status: 'succeeded')
+    payment.update!(status: 'canceled') if payment
+
+    true
+  rescue Stripe::StripeError => e
+    Rails.logger.error("Error canceling subscription: #{e.message}")
+    false
+  end
+end
