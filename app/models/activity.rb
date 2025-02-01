3c6f3c1e3f89f41cb86c6e1137c1d2796b057586
Nadia Kane <nadia.kane26@gmail.com>
2025-01-31 22:59:25 -0600

    Added end_date, removed duration in MVC

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,7 @@
 class Activity < ApplicationRecord
     has_many_attached :images
 
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :date, presence: true
-    validates :location, presence: true
+    validates :title, :description, :start_date, :end_date, :location, presence: true
     validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :cost, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-    validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 end
