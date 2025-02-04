11234f46289d39efdd067a944ca43055a2d492ce
saba n <saba.negeye0@gmail.com>
2025-02-04 15:30:28 -0600

    Update user membership for early member access, update Member Early Access Validations

---
+++ app/models/concerns/early_accessable.rb
@@ -0,0 +1,54 @@
+module EarlyAccessable
+    extend ActiveSupport::Concern
+
+    included do
+      validates :general_registration_start, presence: true
+      validates :early_access_days, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+      validate :end_date_after_start_date
+      validate :general_registration_start_before_start_date
+    end
+
+    def early_registration_start
+      return nil unless general_registration_start.present? && early_access_days.present?
+      general_registration_start - early_access_days.days
+    end
+
+    def early_access_period?
+      return false unless early_access_for_members && early_registration_start.present?
+      Time.current.between?(early_registration_start, general_registration_start)
+    end
+
+    def general_registration_open?
+      general_registration_start.present? && Time.current >= general_registration_start
+    end
+
+    def can_register?(user)
+      return false unless user
+      if user.membership_active?
+        early_access_period? || general_registration_open?
+      else
+        general_registration_open?
+      end
+    end
+
+    def registration_status
+        return "Closed" if Time.current > end_date
+        return "Member Registrations Open, General Registration Opens In:" if early_access_for_members && Time.current < general_registration_start
+        "General registration is now open!"
+      end
+
+
+    def end_date_after_start_date
+      return unless start_date && end_date && end_date < start_date
+      errors.add(:end_date, "can't be earlier than the start date")
+    end
+
+    def general_registration_start_before_start_date
+      return if general_registration_start.nil? || start_date.blank?
+      if self.is_a?(Event) && general_registration_start > start_date
+        errors.add(:general_registration_start, "date can't be later than the event start date")
+      elsif self.is_a?(Activity) && general_registration_start > start_date
+        errors.add(:general_registration_start, "date can't be later than the activity start date")
+      end
+    end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+    include EarlyAccessable
     has_many_attached :images
     has_and_belongs_to_many :users
 
@@ -9,32 +10,9 @@ class Event < ApplicationRecord
     validates :location, presence: true
     validates :rsvp_deadline, presence: true
     validates :cost, numericality: { greater_than_or_equal_to: 0 }
-    validates :early_access_days, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-    validates :general_registration_start, presence: true, if: -> { early_access_for_members? }
+    
 
   # Calculate the early registration start date
-  def early_registration_start
-    return nil unless general_registration_start.present? && early_access_days.present?
-    general_registration_start - early_access_days.days
-  end
-
-  # Check if the event is currently in the early access period
-  def early_access_period?
-    return false unless early_access_for_members && early_registration_start.present?
-    Time.current.between?(early_registration_start, general_registration_start)
-  end
-
-  # Check if general registration is open
-  def general_registration_open?
-    general_registration_start.present? && Time.current >= general_registration_start
-  end
-
-  # Determine if a user can register
-  def can_register?(user)
-    return false unless user
-    return general_registration_open? unless user.member?
-    early_access_period? || general_registration_open?
-  end
 
     has_many :payments, as: :paymentable
 
