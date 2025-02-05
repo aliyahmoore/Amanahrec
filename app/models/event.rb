6f9e45be1fb84e8113c22032f9dbb709f000e35b
Nadia Kane <nadia.kane26@gmail.com>
2025-02-05 08:58:54 -0600

    Updated Events and Activity model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,17 @@
 class Event < ApplicationRecord
-    include EarlyAccessable
-    has_many_attached :images
-    has_and_belongs_to_many :users
+  include EarlyAccessable
+  has_many_attached :images
+  has_and_belongs_to_many :users
 
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :start_date, presence: true
-    validates :end_date, presence: true
-    validates :location, presence: true
-    validates :rsvp_deadline, presence: true
-    validates :cost, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, :description, :start_date, :end_date, :location, :rsvp_deadline, presence: true
+  validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  # Calculate the early registration start date
 
-    # Calculate the early registration start date
+  has_many :payments, as: :paymentable
 
-    has_many :payments, as: :paymentable
-
-    def start_time
-      start_date
-    end
+  def start_time
+    start_date
+  end
 end
