37fe5b44a371c835a290870631b47d011d688c94
peterlee597 <peterlee597@gmail.com>
2025-02-06 06:17:50 -0600

    Added Capacity to Events along with form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_02_233602) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_06_115528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,13 +66,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_233602) do
     t.datetime "general_registration_start"
   end
 
-  create_table "activities_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "activity_id", null: false
-    t.index ["activity_id", "user_id"], name: "index_activities_users_on_activity_id_and_user_id"
-    t.index ["user_id", "activity_id"], name: "index_activities_users_on_user_id_and_activity_id"
-  end
-
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.string "position"
@@ -96,6 +89,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_233602) do
     t.boolean "early_access_for_members", default: false, null: false
     t.integer "early_access_days"
     t.datetime "general_registration_start"
+    t.integer "capacity"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
@@ -103,6 +97,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_233602) do
     t.bigint "event_id", null: false
     t.index ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id"
     t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "published_date"
+    t.string "organization_name"
   end
 
   create_table "media_mentions", force: :cascade do |t|
@@ -190,6 +194,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_02_233602) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "memberships", "users"
   add_foreign_key "payments", "users"
+  add_foreign_key "registrations", "users"
   add_foreign_key "testimonials", "users"
   add_foreign_key "users", "roles"
 end
