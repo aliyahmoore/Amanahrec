7c5d845d72e80d25938b3424a00b758cbfece860
Nadia Kane <nadia.kane26@gmail.com>
2025-02-08 22:56:43 -0600

    ActiveAdmin for Payments

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Payment do
+  actions :index, :show
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,5 +15,46 @@ ActiveAdmin.register Payment do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :stripe_payment_id
+    column :amount, sortable: true do |payment|
+      number_to_currency(payment.amount)
+    end
+    column :status
+    column :is_recurring
+    column :recurring_type
+    column :paymentable_type
+    column :paymentable_id
+    column :created_at
+    column :updated_at
+  end
+
+  show do
+    attributes_table do
+      row :user
+      row :stripe_payment_id
+      row :amount do |payment|
+        number_to_currency(payment.amount)
+      end
+      row :status
+      row :is_recurring
+      row :recurring_type
+      row :paymentable_type
+      row :paymentable_id
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  filter :user
+  filter :status
+  filter :is_recurring
+  filter :recurring_type
+  filter :amount
+  filter :created_at
   
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,13 @@
 class Payment < ApplicationRecord
   belongs_to :paymentable, polymorphic: true
   belongs_to :user
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["amount", "is_recurring", "paymentable_id", "paymentable_type", "recurring_type", "status", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["paymentable", "user"]
+  end
+
 end
