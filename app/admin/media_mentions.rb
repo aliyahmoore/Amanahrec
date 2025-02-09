9deba96f185a3c39656e437ff4b700a1191c5e0e
Nadia Kane <nadia.kane26@gmail.com>
2025-02-08 23:11:12 -0600

    ActiveAdmin for MediaMentions

--- app/admin/media_mentions.rb
+++ app/admin/media_mentions.rb
@@ -14,5 +14,44 @@ ActiveAdmin.register MediaMention do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  permit_params :name, :link, :published_date, :organization_name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :link
+    column :published_date
+    column :organization_name
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :link
+      row :published_date
+      row :organization_name
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :link
+      f.input :published_date, as: :datepicker
+      f.input :organization_name
+    end
+    f.actions
+  end
   
+  filter :name
+  filter :organization_name
+  filter :published_date
+  filter :created_at
+
+
 end
