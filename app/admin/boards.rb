28ce5df06aaa0edd3f47ab7e7b9dd39352b73742
Nadia Kane <nadia.kane26@gmail.com>
2025-02-08 23:17:16 -0600

    ActiveAdmin for Boards

--- app/admin/boards.rb
+++ app/admin/boards.rb
@@ -15,4 +15,50 @@ ActiveAdmin.register Board do
   #   permitted
   # end
   
+  permit_params :name, :position, :description, :image
+
+  # Show view customization
+  show do
+    attributes_table do
+      row :name
+      row :position
+      row :description
+      row :image do |board|
+        if board.image.attached?
+          image_tag board.image.variant(resize_to_limit: [300, 300])
+        else
+          "No image uploaded"
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  # Filters
+  filter :name
+  filter :position
+  filter :created_at
+
+  # Index view customization
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :position
+    column :description
+    column :created_at
+    actions
+  end
+
+  # Form view customization
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :position
+      f.input :description
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
