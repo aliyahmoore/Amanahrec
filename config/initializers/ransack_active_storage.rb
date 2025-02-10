5e332f7c85b13de1483d90d44ac62b1fd7cec28d
Nadia Kane <nadia.kane26@gmail.com>
2025-02-09 21:16:56 -0600

    Resized Events show and activities show

---
+++ config/initializers/ransack_active_storage.rb
@@ -0,0 +1,9 @@
+module ActiveStorage
+    class Attachment < ApplicationRecord
+      def self.ransackable_attributes(auth_object = nil)
+        # Allow these attributes to be searchable
+        ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+      end
+    end
+  end
+  
