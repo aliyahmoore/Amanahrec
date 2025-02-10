8f4406eeebe0cf1240012b63ea82d42c02377d8f
Nadia Kane <nadia.kane26@gmail.com>
2025-02-09 21:49:03 -0600

    Linting error

--- config/initializers/ransack_active_storage.rb
+++ config/initializers/ransack_active_storage.rb
@@ -2,8 +2,7 @@ module ActiveStorage
     class Attachment < ApplicationRecord
       def self.ransackable_attributes(auth_object = nil)
         # Allow these attributes to be searchable
-        ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
+        [ "blob_id", "created_at", "id", "name", "record_id", "record_type" ]
       end
     end
-  end
-  
+end
