7c9a869b450615d6c6e306039a2009eb99911ea0
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 00:12:02 -0600

    Slug back in show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,16 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [ :show ]
     def index
         @activities = Activity.order(start_date: :desc)
     end
-
-
+    
     def show
-        @can_register = @activity.can_register?(current_user)
+      @can_register = @activity.can_register?(current_user)
+    end
+
+    private
+
+    def set_activity
+      @activity = Activity.friendly.find(params[:slug])
     end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,16 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [ :show ]
     def index
         @events = Event.order(start_date: :desc)
     end
+    
     def show
       @can_register = @event.can_register?(current_user)
+    end
+
+    private
+
+    def set_event
+      @event = Event.friendly.find(params[:slug])
     end
 end
