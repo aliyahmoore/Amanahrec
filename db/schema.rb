cf4c054951c5966bff4005310021cd5181456650
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 22:24:58 -0600

    Partner model for logos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_15_051415) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_16_042219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -149,6 +149,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_15_051415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "partners", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "payments", force: :cascade do |t|
