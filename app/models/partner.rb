726b9232af1756f7efb5d329233ba492a5cf1da3
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 22:56:54 -0600

    Our Partners Index style

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,11 @@
 class Partner < ApplicationRecord
     has_one_attached :logo
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "id", "name", "updated_at"]
+      end
+      
+    def self.ransackable_associations(auth_object = nil)
+        ["logo_attachment", "logo_blob"]
+      end
 end
