87da3ef5d68b951a19aacc61e67a51b5ea4509dd
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 23:06:04 -0600

    Sponsor model and active admin initialized

---
+++ db/migrate/20250216050443_create_sponsors.rb
@@ -0,0 +1,9 @@
+class CreateSponsors < ActiveRecord::Migration[7.2]
+  def change
+    create_table :sponsors do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_16_042219) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_16_050443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -184,6 +184,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_16_042219) do
   end
 
   create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
