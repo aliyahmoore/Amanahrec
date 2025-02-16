0b2a1052a5d395fd624f10a8bf0a92a380edd469
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 18:06:59 -0600

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,28 @@
 Rails.application.routes.draw do
+  # Devise authentication
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :boards
   devise_for :users
-  get "pages/home"
-  get "pages/about"
-  get "pages/calendar"
-  get "pages/partners"
-  get "pages/board"
+
+  # Static pages
+  get "pages/home", "pages/about", "pages/calendar", "pages/partners", "pages/board"
+  root "pages#home"
+
+  # User-specific routes
   get "/my_registrations", to: "registrations#my_registrations", as: "my_registrations"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :media_mentions
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root "pages#home"
-  resources :testimonials, only: [ :index, :new, :create, :edit, :update, :destroy ] do
-    member do
-      patch :approve
-      patch :unapprove
-    end
-  end
+  resources :boards, only: [:index, :show]
+  resources :media_mentions, only: [:index, :show]
+
+  resources :testimonials, only: [ :index, :new, :create ] 
 
   concern :registrable do
     resources :registrations, only: [ :create ]
