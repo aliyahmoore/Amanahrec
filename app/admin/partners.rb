2249b743de82245de6bda9fc11d7e0b20fc61494
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 22:30:58 -0600

    Partners for active admin

--- app/admin/partners.rb
+++ app/admin/partners.rb
@@ -1,18 +1,36 @@
 ActiveAdmin.register Partner do
+  permit_params :logo
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+index do
+  column :id
+  column :logo do |partner|
+    if partner.logo.attached?
+      image_tag url_for(partner.logo), size: "100x100"
+    else
+      "No Logo Uploaded"
+    end
+  end
+  actions
 end
+
+  form do |f|
+    f.inputs do
+      f.input :logo, as: :file
+    end
+    f.actions
+end
+
+  show do
+    attributes_table do
+      row :id
+      row :logo do |partner|
+        if partner.logo.attached?
+          image_tag url_for(partner.logo), size: "100x100"
+        else
+          "No Logo Uploaded"
+        end
+      end
+    end
+  end
+end
+
