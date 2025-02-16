08e0204b12ee1085724c74df94f11af82f5885b8
Nadia Kane <nadia.kane26@gmail.com>
2025-02-16 09:52:19 -0600

    Fix test fail

--- test/controllers/sponsor_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SponsorControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get sponsor_index_url
-    assert_response :success
-  end
-end
--- test/controllers/sponsors_controller_test.rb
+++ test/controllers/sponsors_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PartnersControllerTest < ActionDispatch::IntegrationTest
+class SponsorsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
