c99c64f1bf0baa11727e342d504ed38c3fa4d4e7
Nadia Kane <nadia.kane26@gmail.com>
2025-02-15 23:06:47 -0600

    Active Admin Sponsor completed

--- app/admin/sponsors.rb
+++ app/admin/sponsors.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register Sponsor do
+  permit_params :name, :logo
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+index do
+  column :id
+  column :name
+  column :logo do |sponsor|
+    if sponsor.logo.attached?
+      image_tag url_for(sponsor.logo), size: "100x100"
+    else
+      "No Logo Uploaded"
+    end
+  end
+  actions
+end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :logo, as: :file
+    end
+    f.actions
+end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :logo do |sponsor|
+        if sponsor.logo.attached?
+          image_tag url_for(sponsor.logo), size: "100x100"
+        else
+          "No Logo Uploaded"
+        end
+      end
+    end
+  end
 end
