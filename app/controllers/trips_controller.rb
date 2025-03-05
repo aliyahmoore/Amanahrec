904384d62555c6acb700ea2796e879ddbcf11daf
Nadia Kane <nadia.kane26@gmail.com>
2025-03-04 23:16:21 -0600

    Nav tabs for Trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,8 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [ :show ]
     def index
-        @trips = Trip.order(start_date: :desc)
+      @current_trips = Trip.where('start_date >= ?', Date.today).order(:start_date)
+      @past_trips = Trip.where('start_date < ?', Date.today).order(:start_date)
     end
 
     def show
