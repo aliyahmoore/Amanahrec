8865d2802042f867880bf88eff398332bbb8ee34
Nadia Kane <nadia.kane26@gmail.com>
2025-03-04 23:10:22 -0600

    Nav tabs for Activity and Events

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,8 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [ :show ]
     def index
-        @activities = Activity.order(start_date: :desc)
+      @current_activities = Activity.where('start_date >= ?', Date.today).order(:start_date)
+      @past_activities = Activity.where('start_date < ?', Date.today).order(:start_date)
     end
 
     def show
