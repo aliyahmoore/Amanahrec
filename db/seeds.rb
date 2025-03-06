adef67a2dd65be292e8eb544e49d70273b64f430
Nadia Kane <nadia.kane26@gmail.com>
2025-03-06 17:39:52 -0600

    Board image and spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-10.times do
-        user = User.create!(
-          first_name: Faker::Name.first_name,
-          last_name: Faker::Name.last_name,
-          email: Faker::Internet.unique.email,
-          password: 'Password123!',  # Ensure this meets your password complexity rules
-          phone_number: Faker::PhoneNumber.cell_phone_in_e164,
-          gender: [ 'Male', 'Female', 'Non-Binary' ].sample,
-          ethnicity: [ 'White', 'Black or African American', 'Asian', 'American Indian or Alaska Native', 'Native Hawaiian or Other Pacific Islander', 'Hispanic or Latino', 'Other', 'Prefer not to say' ].sample,
-          approved: [ true, false ].sample,  # Random approval status
-          age_range: [ '18-25', '26-35', '36-45', '46-60', '60+' ].sample,  # Random age range
-          created_at: Time.now,
-          updated_at: Time.now
-        )
-        # Confirm the user immediately by setting confirmed_at
-        user.update!(confirmed_at: Time.now)
-      end
