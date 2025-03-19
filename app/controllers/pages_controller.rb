bee882f2daa67dd58fc100b507a9b00ec2294a10
Nadia Kane <nadia.kane26@gmail.com>
2025-03-18 23:21:20 -0500

    Homepage image carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def home
     @testimonials = Testimonial.where(approved: true).order(created_at: :desc)
-    @gallery_images = Dir.glob("app/assets/images/gallery/*").map { |img| ActionController::Base.helpers.asset_path(img.gsub("app/assets/images/", "")) }
-  end
+    @gallery_images = Dir.glob("app/assets/images/gallery/*")
+                     .sort_by { |img| File.basename(img).split('_').first.to_i } 
+                     .map { |img| ActionController::Base.helpers.asset_path(img.gsub("app/assets/images/", "")) }
+end
 
   def calendar
     @trips = Trip.all
