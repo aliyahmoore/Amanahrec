6bc87b3b57b5fd9033731a9d891ad691eedfaa51
Nadia Kane <nadia.kane26@gmail.com>
2025-03-21 21:50:48 -0500

    Full dates and simple format descriptions

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -59,7 +59,9 @@ ActiveAdmin.register Activity do
   show do
     attributes_table do
       row :title
-      row :description
+      row :description do |resource|
+        simple_format(resource.description)
+      end
       row :start_date do |activity|
         activity.start_date.strftime("%B %d, %Y %I:%M %p") if activity.start_date
       end
