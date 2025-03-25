27e90328bd0fab6e1aecae713038356f3b165a38
Nadia Kane <nadia.kane26@gmail.com>
2025-03-25 18:44:43 -0500

    Update date formate

--- config/initializers/init_datetimepicker.rb
+++ config/initializers/init_datetimepicker.rb
@@ -1,6 +1,6 @@
 ActiveAdminDatetimepicker::Base.default_datetime_picker_options = {
-  format: "d/m/Y h:i A", # JS format (12-hour with AM/PM)
+  format: "m/d/Y h:i A", # JS fordat (12-hour with AM/PM)
   defaultTime: proc { Time.current.strftime("%I:00 %p") } # 12-hour format with AM/PM
 }
 
-ActiveAdminDatetimepicker::Base.format = "%d/%m/%Y %I:%M %p" # Ruby format (12-hour with AM/PM)
+ActiveAdminDatetimepicker::Base.format = "%m/%d/%Y %I:%M %p" # Ruby format (12-hour with AM/PM)
