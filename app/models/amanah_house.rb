a2ed83a3f2be105bd70750dca30dba8857678dc8
Nadia Kane <nadia.kane26@gmail.com>
2025-05-02 21:24:25 -0500

    Rubocop

--- app/models/amanah_house.rb
+++ app/models/amanah_house.rb
@@ -3,10 +3,10 @@ class AmanahHouse < ApplicationRecord
     has_one_attached :image
 
     def self.ransackable_associations(auth_object = nil)
-        ["image_attachment", "image_blob"]
+        [ "image_attachment", "image_blob" ]
       end
-    
+
       def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "description", "id", "location", "updated_at"]
+    [ "created_at", "description", "id", "location", "updated_at" ]
     end
 end
